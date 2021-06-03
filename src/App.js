d03c901c903f49e5f9901a6af0ed346336556dbd
Francesc <fbedos@hotmail.com>
2021-06-03 18:31:02 +0200

    exercici2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-
 import './App.css';
+import React, { useState } from 'react'
 import linies from './linies.json'
-import React from 'react'
 
-function App() { 
+function App() {
 
-  const res= linies.map((text) => <p>{text}</p>);
-  
+  const [index, setIndex] = useState(0);
+
+  const next = () => {
+
+    setIndex(index => index + 1);
+
+    if (index === 6) {
+      console.log(index);
+      setIndex(0);
+    }
+
+  }
+
+  const back = () => {
+
+    setIndex(index => index - 1);
+
+    if (index === 0) {
+      console.log(index);
+      setIndex(0);
+    }
+  }
   return (
+
     <div className="App">
-      <h1> {res}</h1>
+      <button onClick={next}>següent</button>
+      <button onClick={back}>enrere</button>
+      <h1>{linies[index].texte}</h1>
+
     </div>
   );
 }
